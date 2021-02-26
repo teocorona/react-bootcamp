e7d4ae2b9c961fd7cbb9d641bf56915359bc1806
Teo Corona <teocorona_90@hotmail.com>
2021-02-25 23:16:00 -0600

    init

---
+++ part1/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Mensaje from './Mensaje.js'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Mensaje color='lightgreen' msg='111 '/>
+      <Mensaje color='lightblue' msg='222 '/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ part1/src/Mensaje.js
@@ -0,0 +1,6 @@
+const Mensaje =(props) => {
+    return <h1 style={{color: props.color}}  >
+      {props.msg}  Hola Mensaje.js</h1>
+}
+
+export default Mensaje
